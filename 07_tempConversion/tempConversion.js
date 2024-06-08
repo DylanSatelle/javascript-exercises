227c8eb0f5d76c84fc3aedaf7967924f1e707161
Dylan Satelle <dylanjsatelle@gmail.com>
2024-06-08 13:04:19 +0100

    ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+    //temp - 32 * 5/9
+    celsius = (input - 32) * 5/9;
+    console.log("celsius var = " + celsius);
+    celsiusFinal = celsius.toFixed(1);
+    console.log("celisusFinal var = " + celsiusFinal);
+    return (Number(celsiusFinal));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+
+  fah = (number * 9/5) + 32;
+  fahFInal = fah.toFixed(1);
+  return (Number(fahFInal));
+
+  //temp * 9/5 + 32
 };
 
 // Do not edit below this line
