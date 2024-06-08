fc36c5656a6120eb21c79b87c31c9d7d88fe28ca
Dylan Satelle <dylanjsatelle@gmail.com>
2024-06-08 11:43:48 +0100

    complete ex. 1 and ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+
+    let newWord = "";
+
+    if (number === 0) {
+        return "";
+    }
+    else if (number >= 1) {
+        for (let i = 0; i < number; i++){
+            newWord = newWord+word;
+            console.log(newWord);
+        }
+        return newWord;
+    }
+
+    else {
+        return "ERROR";
+    }
 
 };
 
+//funct
+
 // Do not edit below this line
 module.exports = repeatString;
+
+
+//
