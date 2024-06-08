8b3dbd7fd1a857298cc84edc4a0fc769db1b8b32
Dylan Satelle <dylanjsatelle@gmail.com>
2024-06-08 11:56:02 +0100

    ex 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let arr = word.split("");
+    console.log(arr.reverse());
+    console.log(arr.join());
+    return (arr.join(""));
 };
 
 // Do not edit below this line
